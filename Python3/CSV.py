9e4d81ed59ef2e2dcab424d65d421125661925a6
Nad3s <87640190+Nad3s@users.noreply.github.com>
2022-05-05 15:36:59 +0200

    Create CSV.py
    Add function read_csv

---
+++ Python3/CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+This script regroup all the function who can be usefull for CSV file (reading (R), writting (W), extract (E)...) 
+Modules used:
+- pandas
+"""
+
+#RE. Result -> Table datas with 1 table by line + X tables by peer of Header/Value
+def read_csv(file):
+  datas,header= [],[]
+  table = pandas.read_csv(file)
+  for column in table:
+     header.append(column)
+      
+  for index, row in table.iterrows():
+    line=[]
+    for i in range(len(header)):
+      couple=[]
+      couple.append(header[i])
+      couple.append(table[header[i]].values[index])
+      line.append(couple)
+      
+    datas.append(line)
+  return datas
