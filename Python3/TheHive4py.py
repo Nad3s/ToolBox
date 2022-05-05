6e92cad9246412d07ebaa478a3632886fea0c7ae
Nad3s <87640190+Nad3s@users.noreply.github.com>
2022-05-05 15:48:45 +0200

    Add function for research template on TH

--- Python3/TheHive4py.py
+++ Python3/TheHive4py.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
 This script regroup all the function usefull for "play" with TheHive in CLI.
 Modules used:
@@ -7,3 +8,23 @@ Modules used:
 - requests
 - __future__
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+import json
+from thehive4py.api import TheHiveApi
+from thehive4py.models import Case, CaseTemplate
+from thehive4py.exceptions import CaseTemplateException
+
+#Search if the template's name entered by the user exists in the instance
+def check_template(url,key):
+	api= TheHiveApi(url,key)
+	name=input("What is the name of the template ?: ")
+	print("Research of existing case template")
+
+  try:
+		response=api.get_case_template(name)
+		print(json.dumps(template, indent=4))
+	except CaseTemplateException as e:
+		print(str(e))
